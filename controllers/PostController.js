0f3dc5547070d525fec4fc9d8a3de65ff5aeb648
neto <euneto2501@gmail.com>
2023-02-23 16:35:16 -0300

    Feature: filtragem de posts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,17 +1,41 @@
 const Post = require('../models/Post')
 const User = require('../models/User')
 
+const {Op} = require('sequelize')
+
 module.exports = class PostController {
+  static async showPosts(req, res) {
+    let search = "";
 
-  static async showPosts(req, res){
-    const postsData = await Post.findAll({include: User,})
+    if (req.query.search) {
+      search = req.query.search;
+    }
 
-    const posts = postsData.map((result) => result.get({plain: true}))
-    res.render('posts/home', {posts})
+    let order = 'DESC'
+
+    req.query.order === 'old' ? order = 'ASC' : order = 'DESC';
+
+    const postsData = await Post.findAll({
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [["createdAt", order]],
+    });
+
+    const posts = postsData.map((result) => result.get({ plain: true }));
+   
+    let postsQty = posts.length
+
+    if (postsQty === 0) {
+      postsQty = false
+    }
+
+    res.render("posts/home", { posts, search, postsQty });
   }
 
-  static async showDashboard(req, res){
-    const userId = req.session.userid
+  static async showDashboard(req, res) {
+    const userId = req.session.userid;
 
     const user = await User.findOne({
       where: { id: userId },
@@ -19,90 +43,87 @@ module.exports = class PostController {
       plain: true,
     });
 
-    if(!user){
-      res.redirect('/login')
+    if (!user) {
+      res.redirect("/login");
     }
 
+    const posts = user.Posts.map((result) => result.dataValues);
 
-    const posts = user.Posts.map((result) => result.dataValues)
+    let emptyPosts = false;
 
-    let emptyPosts = false
-
-    if(posts.length === 0){
-      emptyPosts = true
+    if (posts.length === 0) {
+      emptyPosts = true;
     }
 
-    res.render('posts/dashboard', {posts, emptyPosts})
-  }
-  
-  static createPost(req, res){
-    res.render('posts/create')
+    res.render("posts/dashboard", { posts, emptyPosts });
   }
 
-  static async createPostSave(req, res){
+  static createPost(req, res) {
+    res.render("posts/create");
+  }
 
+  static async createPostSave(req, res) {
     const post = {
       title: req.body.title,
       description: req.body.description,
       UserId: req.session.userid,
-    }
+    };
 
     try {
-      await Post.create(post)
+      await Post.create(post);
 
-      req.flash('message', 'Seu post está no ar! :)')
+      req.flash("message", "Seu post está no ar! :)");
 
       req.session.save(() => {
-        res.redirect('/posts/dashboard')
-      })
+        res.redirect("/posts/dashboard");
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
   }
 
-  static async removePost(req, res){
-    const id = req.body.id
-    const userId = req.session.userid
+  static async removePost(req, res) {
+    const id = req.body.id;
+    const userId = req.session.userid;
 
     try {
-      await Post.destroy({where: {id: id, UserId: userId}})
+      await Post.destroy({ where: { id: id, UserId: userId } });
 
-      req.flash('message', 'Sua postagem foi removida com sucesso!')
+      req.flash("message", "Sua postagem foi removida com sucesso!");
 
       req.session.save(() => {
-        res.redirect('/posts/dashboard')
-      })
+        res.redirect("/posts/dashboard");
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
-  static async updatePost(req, res){
-    const id = req.params.id
+  static async updatePost(req, res) {
+    const id = req.params.id;
 
-    const post = await Post.findOne({where: {id: id}, raw: true})
+    const post = await Post.findOne({ where: { id: id }, raw: true });
 
-    res.render('posts/edit', {post})
+    res.render("posts/edit", { post });
   }
 
-  static async updatePostSave(req, res){
-    const id = req.body.id
+  static async updatePostSave(req, res) {
+    const id = req.body.id;
 
     const post = {
       title: req.body.title,
       description: req.body.description,
-    }
+    };
 
     try {
-      await Post.update(post, {where: {id: id}})
-      req.flash('message', 'Você atualizou o seu post! :)')
+      await Post.update(post, { where: { id: id } });
+      req.flash("message", "Você atualizou o seu post! :)");
 
       req.session.save(() => {
-        res.redirect('/posts/dashboard')
-      })
+        res.redirect("/posts/dashboard");
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
-}
+};
